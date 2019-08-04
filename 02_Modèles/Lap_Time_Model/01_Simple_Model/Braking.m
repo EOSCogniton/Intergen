310f656ac5a06e938b87a18ae6017d6fade323f5
bob <bob.aubouin@gmail.com>
2019-08-04 23:35:30 +0200

    3 sous modèles OK

--- 02_Modèles/Lap_Time_Model/01_Simple_Model/Braking.m
+++ 02_Modèles/Lap_Time_Model/01_Simple_Model/Braking.m
@@ -1,26 +1,29 @@
-%% Model
+%% Braking Model
     % Modèle de freinage simpliste pour modèle de temps au tour,
-    % voir la page 16 du cour de dynamique véhicul de l'epsac
+    % Voir la page 16 du cour de dynamique véhicul de l'epsac
     % Input :   - global :g rho Vehicle_file step
     %           - Initial and final speed (km/h)
 
     %Output :   - vectors of speed, time and distance
-    
+
     %Hypothesis :   - longitudinal grip constant
     %               - tire always at the slip limit
- 
 
-    %% Algo
-    %__init__
-    V = [Vi/3.6]; %vitesse du véhicule en m/s
-    d = [0]; %distance parcouru en m
-    t = [0]; % temps écoulé en s
-    i = 1;
-    %__loop__
-    while (V(end)>Vo/3.6) && (t(end)<2) %&& (i<50)
-        d =[d d(end)+step*V(end)] ;
-        a = (-(m_t*g+rho*S*Cz*(V(end)/3.6)^2)*Long_tire_grip-rho*S*Cx*(V(end)/3.6)^2)/m_t ;
-        V = [V V(end)+step*a] ;
-        t = [t t(end)+step] ;
-        %i = i+1 ;
-    end
+
+%% Algo
+%__init__
+V_braking = [Vi/3.6]; %vitesse du véhicule en m/s
+d = [0]; %distance parcouru en m
+t = [0]; % temps écoulé en s
+%__loop__
+while (V_braking(end)>Vo/3.6) && (t(end)<3) 
+    d =[d d(end)+step*V_braking(end)] ;
+    a = (-(m_t*g+1/2*rho*S*Cz*(V_braking(end)/3.6)^2)*Long_tire_grip-1/2*rho*S*Cx*(V_braking(end)/3.6)^2)/m_t ;
+    V_braking = [V_braking V_braking(end)+step*a] ;
+    t = [t t(end)+step] ;
+end
+
+V_braking(end) = Vo; %Correction of the discret calcul to get the right final speed
+V_braking=V_braking*3.6; %Speed in km/h
+
+ 
